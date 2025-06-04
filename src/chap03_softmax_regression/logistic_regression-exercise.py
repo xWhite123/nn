b67c84ae7ebe08a805e6d17edc3c51e2cef6061f
xWhite123 <3013466599@qq.com>
2025-06-04 16:51:55 +0800

    Update logistic_regression-exercise.py

--- src/chap03_softmax_regression/logistic_regression-exercise.py
+++ src/chap03_softmax_regression/logistic_regression-exercise.py
@@ -8,21 +8,18 @@
 # In[7]:
 import tensorflow as tf
 import matplotlib.pyplot as plt
-
 from matplotlib import animation, rc
 from IPython.display import HTML
 import matplotlib.cm as cm
 import numpy as np
 
-# 设置随机种子（确保结果可复现）
-np.random.seed(42)      # NumPy的随机种子
-tf.random.set_seed(42)  # TensorFlow的随机种子
+np.random.seed(42)     
+tf.random.set_seed(42)  
 
-# 确保在Jupyter Notebook中内联显示图形
+
 get_ipython().run_line_magic('matplotlib', 'inline')
 
-# 设置数据点数量
-dot_num = 100
+
 # 从均值为3，标准差为1的高斯分布中采样x坐标，用于正样本
 x_p = np.random.normal(3., 1, dot_num)
 # 从均值为6，标准差为1的高斯分布中采样y坐标，用于正样本
